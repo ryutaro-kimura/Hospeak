aa7e1dd3d4e374703803205abc0fd93e0bd4968d
ryutaro-kimura <sasuker2264@gmail.com>
2022-03-28 13:27:46 +0900

    :package:   CSSが崩れそうなので一旦削除
    Closes #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,35 +3,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  important: true,
-  // Active dark mode on class basis
-  darkMode: "class",
-  i18n: {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
-  },
-  purge: {
-    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
-    // These options are passed through directly to PurgeCSS
-  },
   theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        check: "url('/icons/check.svg')",
-        landscape: "url('/images/landscape/2.jpg')",
-      }),
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["checked"],
-      borderColor: ["checked"],
-      inset: ["checked"],
-      zIndex: ["hover", "active"],
-    },
+    extend: {},
   },
   plugins: [],
-  future: {
-    purgeLayersByDefault: true,
-  },
+  darkMode: 'class'
 }
